d39031230ec09a0bfbb0b07b8dce30e72017177f
Jiyeon <a1919361@gmail.com>
2022-02-25 10:58:47 +0900

    controller, module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
